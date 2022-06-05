ccff65d333931d89b011a3d5552519a6d7e2eef0
Anirudh Verma <vermajianirudh369@outlook.com>
2022-06-05 18:40:30 +0530

    Add project files.

---
+++ CardPickerUI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace CardPickerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+            int value = (int)numberOfCards.Value;
+            HashSet<string> cards = CardPickerUI.CardPicker.RandomCardPicker(value);
+            foreach (string card in cards)
+            {
+                listBox.Items.Add(card);
+            }
+        }
+    }
+}
